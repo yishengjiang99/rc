38535bfaad2c8c4deb6096c4ddb226150379888b
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-06-04 11:56:36 -0700

    d

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Date from '../components/date';
-
-export default function Home({allPostsData}) {
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
+import FileSystemNavigator from "../components/tree";
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>Grep|Awk</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
@@ -29,17 +29,16 @@ export default function Home({allPostsData}) {
           ))}
         </ul>
       </section>
-
+      <FileSystemNavigator />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
-
+      allPostsData,
+    },
+  };
 }

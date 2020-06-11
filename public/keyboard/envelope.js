4e425149010f83b2f84ec3e93625a04fe9a49f7e
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-06-11 00:31:10 -0700

    prog

--- public/keyboard/envelope.js
+++ public/keyboard/envelope.js
@@ -16,17 +16,19 @@ export default function Envelope(
   this.decay = decay;
   this.param = param;
 }
-Envelope.prototype.trigger = function (time) {
+Envelope.prototype.trigger = async function (time) {
   this.attackTime = time + this.attack; //reach attach val at attackTime
   this.decayTime = time + this.attack + this.decay; //reach decay val at decayTime
   this.param.cancelScheduledValues(time);
   this.param.setValueAtTime(0, time);
   this.param.linearRampToValueAtTime(this.max, time + this.attack);
+  await sleep(this.attack - 0.001);
   this.param.linearRampToValueAtTime(
     this.sustainLevel,
     time + this.attack + this.decay
   );
-  this.param.cancelAndHoldAtTime(time + this.attack + this.decay);
+  await sleep(this.decay);
+  this.param.setTargetAtTime(0.001, this.decayTime, this.release);
 };
 Envelope.prototype.hold = function (time) {
   this.param.cancelAndHoldAtTime(time + this.attack + this.decay);
@@ -38,7 +40,6 @@ Envelope.prototype.triggerRelease = async function (time) {
   if (time < this.attackTime) {
     await sleep(this.attackTime - time);
     extraWait = this.attackTime - time;
-    _console.log(extraWait, "extrawait");
   }
 
   this.param.setTargetAtTime(

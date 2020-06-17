a8af24c0f23ae5a14aa9c0dcee113aeeb1531d14
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-06-16 22:52:01 -0700

    d

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ module.exports = {
   env: {
     PG_CONNNECTION_STRING: "postgresql://postgres:welcome@localhost:5432/grepawk",
     google_key: "AIzaSyBCXMcymaqef8RmYskmdVOJcQA5e06Zvyg",
-    useFile
   }
 };
-//PG_CONNNECTION_STRING=postgresql://postgres:welcome@localhost:5432/grepawk
-export AZURE_STORAGE_CONNECTION_STRING=DefaultEndpointsProtocol=https;AccountName=grepaudio;AccountKey=FBlsD/jj3kJT8TnpJfknf4MibbS+W1ri7r7UCSoUtcTAjrnBnLOAD8HxPawTN6tMSfgyKY601rIwlRL/gx0AOA==;EndpointSuffix=core.windows.net
-DefaultEndpointsProtocol=https;AccountName=grepaudio;AccountKey=FBlsD/jj3kJT8TnpJfknf4MibbS+W1ri7r7UCSoUtcTAjrnBnLOAD8HxPawTN6tMSfgyKY601rIwlRL/gx0AOA==;EndpointSuffix=core.windows.net

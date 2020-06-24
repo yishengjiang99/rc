242cf2518f632dde4c654093381f3043d8603acc
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-06-24 01:41:39 -0700

    savev

--- public/keyboard/envelope.js
+++ public/keyboard/envelope.js
@@ -35,17 +35,12 @@ Envelope.prototype.hold = function (time) {
 Envelope.prototype.triggerRelease = async function (time) {
   this.param.cancelScheduledValues(time);
   let extraWait = 0;
-  if (time < this.attackTime) {
-    await sleep(this.attackTime - time);
-    extraWait = this.attackTime - time;
-    _console.log(extraWait, "extrawait");
-  }
+  // if (time < this.attackTime) {
+  //   await sleep(this.attackTime - time);
+  //   extraWait = this.attackTime - time;
+  // }
 
-  this.param.setTargetAtTime(
-    0.000001,
-    time + extraWait,
-    this.release + extraWait
-  );
+  this.param.setTargetAtTime(0.000001, time + extraWait, this.release);
 };
 
 function sleep(sec) {

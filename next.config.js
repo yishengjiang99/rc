5b2fc287e7872352fbd38f8aae8319371e33158d
Yisheng Jiang <yishengjiang@yahoo.com>
2020-07-01 15:53:50 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  /* config options here */
-  env: {
-    google_key: "AIzaSyBCXMcymaqef8RmYskmdVOJcQA5e06Zvyg",
-    AZURE_STORAGE_CONNECTION_STRING:
-      "DefaultEndpointsProtocol=https;AccountName=cs4100320004784bc16;AccountKey=67ofwej9wGp0iH9B4rjFq7gAgYsCkOT96zkUmxkm7IBK08XZ77XbsKbHb7RqNc0lodhRBqOFsfPQ9bZWwhGHIA==;EndpointSuffix=core.windows.net",
-  },
-};
